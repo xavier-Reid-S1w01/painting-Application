173f98040b39e9b3439f1f7bbdf4591ac9b8a747
xavier-Reid-S1w01 <23193@burnside.school.nz>
2026-05-08 14:10:12 +1200

    Rodkis

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template,g
+import sqlite3 
 
 app = Flask(__name__)
 
-@app.route('/')
+DATABASE = 'database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.route("/")
 def home():
-    #Home page
-    return "Title"
+    Paintings = query_db("SELECT * FROM Paintings")
+    return render_template("home.html", Paintings=Paintings)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
