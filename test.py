ddeea1eda42e3b3a93867a8590ad8ac1530cb9d1
xavier-Reid-S1w01 <23193@burnside.school.nz>
2026-07-24 10:07:49 +1200

    dw

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ def home():
 
 @app.route('/artist')
 def artist():
-    # Fetch artists AND the first painting's ImageURL for each artist
+    # Fetch artists AND the first painting's ImageURL as cover image
     all_artists = query_db('''
         SELECT Artist.ArtistID,
                Artist.ArtistName,
@@ -44,15 +44,7 @@ def artist():
         GROUP BY Artist.ArtistID
     ''')
 
-    # Filter paintings if an artist link was clicked
-    selected_artist_id = request.args.get('artist_id')
-
-    if selected_artist_id:
-        paintings = query_db("SELECT * FROM Paintings WHERE ArtistID = ?", [selected_artist_id])
-    else:
-        paintings = query_db("SELECT * FROM Paintings")
-
-    return render_template("artist.html", artists=all_artists, Paintings=paintings)
+    return render_template("artist.html", artists=all_artists)
 
 
 @app.route('/artworks')
@@ -76,6 +68,40 @@ def artworks():
         paintings = query_db("SELECT * FROM Paintings")
 
     return render_template("artworks.html", movements=all_movements, Paintings=paintings)
+
+@app.route('/filter')
+def filter_page():
+    # Read the parameter from the URL bar
+    selected_movement_id = request.args.get('movement_id')
+    selected_artist_id = request.args.get('artist_id')
+
+    #  Query the database depending on what was clicked
+    if selected_movement_id:
+        # Fetch paintings for the clicked movement
+        paintings = query_db("SELECT * FROM Paintings WHERE MovementID = ?", [selected_movement_id])
+        # Fetch the movement details for a page title heading
+        category_title = query_db("SELECT MovementName FROM ArtMovement WHERE MovementID = ?", [selected_movement_id], one=True)
+        title = category_title['MovementName'] if category_title else "Filter Results"
+
+    elif selected_artist_id:
+        # Fetch paintings by the clicked artist
+        paintings = query_db("SELECT * FROM Paintings WHERE ArtistID = ?", [selected_artist_id])
+        # Fetch the artist details for a page title heading
+        category_title = query_db("SELECT ArtistName FROM Artist WHERE ArtistID = ?", [selected_artist_id], one=True)
+        title = category_title['ArtistName'] if category_title else "Filter Results"
+
+    else:
+        # Fallback: if no query parameter is passed
+        paintings = query_db("SELECT * FROM Paintings")
+        title = "All Paintings"
+
+    # Pass the filtered results and title into filter.html
+    return render_template("filter.html", Paintings=paintings, page_title=title)
+
+@app.route('/painting/<int:painting_id>')
+def painting_detail(painting_id):
+    painting = query_db("SELECT * FROM Paintings WHERE PaintingID = ?", [painting_id], one=True)
+    return render_template("painting_detail.html", painting=painting)
 
 
 if __name__ == "__main__":
